263deb86c69d41902bcd480afb9a4080381d1ea2
ogutu333 <ogutu333@gmail.com>
2024-01-18 20:35:05 +0000

    Error handler: Unauthorized

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -25,3 +25,18 @@ def stats() -> str:
     stats['users'] = User.count()
     return jsonify(stats)
 
+
+@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
+def unauthorized() -> str:
+    """ GET /api/v1/unauthorized
+    Return - raises 401 error
+    """
+    abort(401)
+
+
+@app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
+def forbidden() -> str:
+    """ GET /api/v1/forbidden
+    Return - raises 403 error
+    """
+    abort(403)
